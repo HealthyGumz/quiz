6a6e5493d279f35f1c5cd802182c802b8577f98c
vadymdonner <87780479+vadymdonner@users.noreply.github.com>
2021-08-13 17:43:22 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 var currentTab = 0;
 let arrStepHeaders = ['About Me', 'Lifestyle', 'Dental Health', 'Warning Signs I', 'Warning Signs II', 'Health History'];
-var width=0;
+var width = 0;
 
 
 function init() {
 
-  width=document.body.clientWidth; 
+  width = document.body.clientWidth;
 
-  if (width>767){
+  if (width > 767) {
     $('#curved').elipText({ radius: 265 });
   }
-  else{
+  else {
     $('#curved').elipText({ radius: 160 });
   }
 
@@ -19,13 +19,29 @@ function init() {
     element.addEventListener("click", checkBoxClick, false);
   });
 
+  $('.disclaimer').click(function () {
+    $('.down-arrow').toggleClass('invert');
+    $('.disclaimer-text').slideToggle(300);
+    // return false;
+  });
+
+
+  $('.disclaimer-finish').click(function () {
+    $('.down-arrow-finish').toggleClass('invert');
+    // $('.step-content').toggleClass('invert-finish');
+    // $('#stepActions').toggleClass('invert-finish');
+    $('.disclaimer-text-finish').slideToggle(500);
+    // return false;
+  });
+
+
   var x = document.getElementsByClassName("form-step");
 
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
 
- 
+
 }
 
 function checkBoxClick() {
@@ -40,29 +56,36 @@ function checkBoxClick() {
 
 
 function showTab(n) {
+
+
   var x = document.getElementsByClassName("form-step");
   $("#form_step-" + parseInt(n + 1)).fadeIn(800);
   x[n].style.display = "flex";
 
-  if (n == 0) {
+  if (n == 5) {
+    document.getElementById("stepActionsFinish").style.visibility = "visible";
+    document.getElementById("stepActions").style.visibility = "hidden";
+  }
+  else {
+    document.getElementById("stepActionsFinish").style.visibility = "hidden";
+    document.getElementById("stepActions").style.visibility = "visible";
+  }
+
+  if (n == 0 || n==5) {
     document.getElementById("prevBtn").style.visibility = "hidden";
-  } else {
+  }
+  else  {
     document.getElementById("prevBtn").style.visibility = "visible";
   }
 
 
-  if (n == 5) {
-    document.getElementById("nextBtn").innerHTML = "Finish";
-  } else {
-    document.getElementById("nextBtn").innerHTML = "Next";
-  }
 
   document.getElementById("stepHeaders").innerHTML = arrStepHeaders[n];
 
   document.getElementById("stepIndex").innerHTML = "Step " + (n + 1) + " /6";
 
-  var target = document.getElementById ('mainCaption');
-  $('html, body').animate({scrollTop: $(target).offset().top-180}, 10);
+  var target = document.getElementById('mainCaption');
+  $('html, body').animate({ scrollTop: $(target).offset().top - 180 }, 10);
 
   fixStepIndicator(n)
 }
@@ -108,11 +131,12 @@ function nextPrev(n) {
 
     currentTab = currentTab + n;
 
-    if (n == - 1) {
-      document.getElementById("nextBtn").innerHTML = "Next";
-    }
+    // if (n == - 1) {
+    //   document.getElementById("nextBtn").innerHTML = "Next";
+    // }
 
-    if (document.getElementById("nextBtn").innerHTML == "Finish") {
+    // if (document.getElementById("stepActionsFinish").style.visibility == "visible") {
+    if (currentTab == 6) {
       checkResult();
     }
     else {
@@ -120,6 +144,9 @@ function nextPrev(n) {
     }
   }
 }
+
+
+
 
 function navClick(currentNav) {
   // var x = document.getElementsByClassName("form-step");
@@ -180,14 +207,14 @@ function checkResult() {
   resultStep.style.display = "block";
 
 
-  if (width>767){
+  if (width > 767) {
     document.getElementById("stepActionsResult").style.display = "block";
   }
-  else{
+  else {
     document.getElementById("stepActionsResult").style.display = "flex";
   }
 
-    document.getElementById("stepActions").style.display = "none";
+  document.getElementById("stepActions").style.display = "none";
 
   var nav = document.getElementsByClassName("nav-step")
   nav[nav.length - 1].className = nav[nav.length - 1].className += " current";
@@ -197,30 +224,26 @@ function checkResult() {
   // }
 
   document.getElementById("stepIndex").innerHTML = "Step 6/6";
-  document.getElementById("stepHeaders").innerHTML="Result";
+  document.getElementById("stepHeaders").innerHTML = "Result";
 
 
   if (result <= 3) {
     result = 5;
-    resultlDescription = 'Great News! Your score suggests that you are at low risk of having gum disease.  By maintaining a low score, you can reduce the likelihood of developing other  related medical conditions. To learn more, click on one of the buttons below:';
+    resultlDescription = 'Great News! Your score suggests that you may be at low risk of having gum disease. By maintaining a low score, you can reduce the likelihood of developing other  related medical conditions. To learn more, click on one of the buttons below:';
   }
   else if (result > 3 && result <= 8) {
     result = 15;
-    resultlDescription = 'Your score suggests that you are at moderate risk of having gum disease. Patients with gum disease are more likely to develop related chronic medical conditions. Your score indicates the chance that your gum health will get worse without professional care. To take the first step towards having better oral and overall health, click on the links below:';
+    resultlDescription = 'Your score suggests that you may be at moderate risk of having gum disease. Patients with gum disease are more likely to develop related chronic medical conditions. Your score indicates the chance that your gum health will get worse without professional care. To take the first step towards having better oral and overall health, click on the links below:';
   }
   else {
     result = 25;
-    resultlDescription = 'Your score suggests that you are at high risk of having gum disease. Patients with gum disease are more likely to develop related chronic medical conditions.  Your score implies that your gum health will get worse without professional care. To take action towards having better oral and overall health, click on the links below:';
+    resultlDescription = 'Your score suggests that you may be at high risk of having gum disease. Patients with gum disease are more likely to develop related chronic medical conditions.  Your score implies that your gum health will get worse without professional care. To take action towards having better oral and overall health, click on the links below:';
   };
 
 
   initQauge(result);
 
   document.getElementById("textResult").innerHTML = resultlDescription;
-  
-  var target = document.getElementById ('mainCaption');
-  $('html, body').animate({scrollTop: $(target).offset().top-180}, 10);
-
 }
 
 
@@ -306,7 +329,7 @@ function validateForm(currentTab) {
 
 function fixStepIndicator(n) {
 
-  var i, x = document.getElementsByClassName("form-step");
+  var x = document.getElementsByClassName("form-step");
   var nav = document.getElementsByClassName("nav-step")
 
   for (i = 0; i < x.length; i++) {
@@ -321,6 +344,70 @@ function fixStepIndicator(n) {
   for (i = 0; i <= n; i++) {
     nav[i].className = nav[i].className += " current";
   }
+}
+
+
+function validateMail() {
+
+  var isValidate = true;
+
+  var name = document.getElementById("inputName");
+  var mail = document.getElementById("inputMail");
+
+  if (name.value == "") {
+    isValidate = false;
+    name.classList.add("error-circle-mail");
+  }
+  else {
+    isValidate = true;
+    name.classList.remove("error-circle-mail");
+  }
+
+  if (mail.value == "") {
+    isValidate = false;
+    mail.classList.add("error-circle-mail");
+  }
+  else {
+    isValidate = true;
+    mail.classList.remove("error-circle-mail");
+  }
+
+  return isValidate;
 
 }
 
+
+function sendMail() {
+
+  if (!validateMail()) return;
+
+  emailjs.init("user_8Cr1IxiKSaA9gSvYkmq9Q");
+
+  var name = document.getElementById("inputName").value;
+  var mail = document.getElementById("inputMail").value;
+  var zipCode = document.getElementById("inpuZipCode").value;
+
+  var data = {
+    service_id: 'service_wrag93q',
+    template_id: 'template_3xzeugn',
+    user_id: 'user_8Cr1IxiKSaA9gSvYkmq9Q',
+    template_params: {
+      'from_name': 'HealthyGumz',
+      'to_name': name,
+      'to_mail': mail,
+      'g-recaptcha-response': '03AHJ_ASjnLA214KSNKFJAK12sfKASfehbmfd...'
+    }
+  };
+
+  $.ajax('https://api.emailjs.com/api/v1.0/email/send', {
+    type: 'POST',
+    data: JSON.stringify(data),
+    contentType: 'application/json'
+  }).done(function () {
+    // alert('Your mail is sent!');
+  }).fail(function (error) {
+    // alert('Oops... ' + JSON.stringify(error));
+  });
+
+
+}
